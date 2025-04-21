3ab63fa471d4277beb0375e07423885d9244ac7b
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-04-21 12:08:12 +0100

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,6 +1,6 @@
 const pool = require('../../database/db');
 const path = require('path');
-console.log(path.resolve(__dirname, '../db'));
+console.log(path.resolve(__dirname, '../../database/db'));
 
 
 const acct =  async (req, res) => {
