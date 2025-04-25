158d1abb08aa1dae88479ccb34b35daecd70a496
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-04-25 11:19:46 +0100

    d

--- frontend/src/features/inboxSlice.js
+++ frontend/src/features/inboxSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const baseURL = process.env.REACT_APP_API_URL || 'https://thriftwebsite.fly.dev';
 
 export const getChatId = createAsyncThunk('inbox/getChatId', async (userId) => {
     const response = await fetch(`${baseURL}/inbox/getChatId/${userId}`, {
